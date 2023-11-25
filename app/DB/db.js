6baa7e394611ac21c2508ae616e3d068aecb292d
chau21577 <chau21577@gmail.com>
2023-11-25 11:01:48 +0700

    fix config

--- app/DB/db.js
+++ app/DB/db.js
@@ -3,10 +3,10 @@ import mysql from "mysql2/promise";
 export async function query({ query, values = [] }) {
   const dbconnection = await mysql.createConnection({
     host: "localhost",
-    database: "products",
+    database: "magic_post",
     // port: 8889,
     user: "root",
-    password: "password",
+    password: "1",
     // socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
   });
   try {
