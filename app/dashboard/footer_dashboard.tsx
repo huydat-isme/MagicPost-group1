3155d0d69d6e3b06adfe87039c9b243f18770620
huydat-isme <datprono28@gmail.com>
2023-12-05 13:29:19 +0700

    update dashboard

---
+++ app/dashboard/footer_dashboard.tsx
@@ -0,0 +1,9 @@
+export default function Footer_dashboard(){
+return (
+    <footer className="footer px-4 py-6">
+              <div className="footer-content">
+                <p className="text-sm text-gray-600 text-center">Magic post 2023 <a href="https://twitter.com/iaminos">by group 1</a></p>
+              </div>
+            </footer>
+)
+}
