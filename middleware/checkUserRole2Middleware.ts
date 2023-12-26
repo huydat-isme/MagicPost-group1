1d9b1f2b8207b4a118e086ae8013f4c77b8b43ae
chau21577 <chau21577@gmail.com>
2023-12-26 19:29:32 +0700

    Middleware check for role 1 and 2

---
+++ middleware/checkUserRole2Middleware.ts
@@ -0,0 +1,18 @@
+// checkUserRoleMiddleware.ts
+import { NextApiRequest, NextApiResponse } from "next";
+
+const checkUserRoleMiddleware = (func: any) => async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  next: () => void
+) => {
+  const userRole = req.headers["role"];
+
+  if (userRole === "2" || userRole === "1" ) {
+    await func(req, res); // Sử dụng 'await' để đảm bảo middleware hoàn tất trước khi gọi 'next'
+  } else {
+    res.status(403).json({ message: "Forbidden - No permission" });
+  }
+};
+
+export default checkUserRoleMiddleware;
